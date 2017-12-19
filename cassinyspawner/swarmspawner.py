fe4f7e02af45d35324213ad9f730278a2affbbd6
Jesse van der Sar <jesse@sar.nl>
2017-12-19 02:19:20 +0100

    Added exam name to service name

--- cassinyspawner/swarmspawner.py
+++ cassinyspawner/swarmspawner.py
@@ -141,10 +141,13 @@ class SwarmSpawner(Spawner):
         else:
             server_name = 1
 
-        return "{}-{}-{}".format(self.service_prefix,
-                                 self.service_owner,
-                                 server_name
-                                 )
+        #return "{}-{}-{}".format(self.service_prefix,
+        #                         self.service_owner,
+        #                         server_name
+        #                         )
+        
+        return "{}-{}".format(self.user.exam,
+                                 self.service_owner)
 
     def load_state(self, state):
         super().load_state(state)
